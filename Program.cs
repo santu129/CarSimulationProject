19d142f23470267ea3551cdc1f11bea27a9a038c
santu129 <154011371+santu129@users.noreply.github.com>
2023-12-16 23:07:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CarSimulationProject.Domain
+{
+
+
+    public class Program
+    {
+        static void Main()
+        {
+            // Read input
+
+            string[] fieldSize = Console.ReadLine().Split(' ');
+
+            int width = int.Parse(fieldSize[0]);
+            int height = int.Parse(fieldSize[1]);
+
+            string[] initialPosition = Console.ReadLine().Split(' ');
+            int x = int.Parse(initialPosition[0]);
+            int y = int.Parse(initialPosition[1]);
+            char direction = char.Parse(initialPosition[2]);
+
+            string commands = Console.ReadLine();
+
+            // Process commands
+            var car = new Domain.AutoDrivingCarSimulator(x, y, direction);
+            var result =car.ExecuteCommands(commands, width, height);
+
+            // Output final position and direction
+            Console.WriteLine($"Final position:({result.Item1},{result.Item2}) , Final direction: {result.Item3}");
+           
+            Console.ReadLine();
+        }
+    }
+
+}
+
+
